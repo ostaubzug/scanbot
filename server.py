8b7c94e9f1560289daacc202ea5a6d1fe13d40e1
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-05 23:27:43 +0100

    web app working now

--- server.py
+++ server.py
@@ -6,11 +6,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+
+
 @app.route('/scanfunction', methods=['POST'])
 def scan_function():
-    data = request.get_json()  # Read the request body as JSON
+    #data = request.get_json()
+    app.logger.info(request.get_json().get('type'))
 
-    return data
+    return "Scan Successfull"
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=5400, debug=True)
