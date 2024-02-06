fba72f8a39fadd1957303364097fd023f51c8cc0
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-06 18:17:53 +0100

    download is working !

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_file
 import glob
 
 app = Flask(__name__)
@@ -10,19 +10,24 @@ def home():
 @app.route('/scanfunction', methods=['POST'])
 def scan_function():
     app.logger.info(request.get_json().get('type'))
-
-    return createReturnHtml()
-
-def createReturnHtml():
     pdf_files = glob.glob("scanRessources/*.pdf")
+    
     html = ""
     for file in pdf_files:
-        filename = file.split('/')[-1]
-        html += createDownloadCardForPdf(filename)
+        html += createDownloadCardForPdf(file)
     return html
 
-def createDownloadCardForPdf(file_name: str):
-    return f'<article><header>{file_name}</header><button role="button" class="secondary">Download</button></article>'
+def createDownloadCardForPdf(path: str):
+    file_name = path.split('/')[1]
+    return f'<article><header>{file_name}</header><button role="button" class="secondary" onclick="download(\'{path}\')">Download</button></article>'
+
+
+@app.route('/download', methods=['POST'])
+def download():
+    filepath = request.get_json().get('filepath')
+    app.logger.info(filepath)
+    return send_file(filepath, as_attachment=True)
+
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=5400, debug=True)
