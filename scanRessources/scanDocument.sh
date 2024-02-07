ae540d27e58d21b17b9980505b56b516dbea4091
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-07 14:08:33 +0100

    added append

---
+++ scanRessources/scanDocument.sh
@@ -0,0 +1,4 @@
+https://github.com/rocketraman/sane-scan-pdf
+
+device erkennen können mit dem Device listing feature
+ansonsten einen Fehler ausgeben
--- server.py
+++ server.py
@@ -26,8 +26,12 @@ def createDownloadGrid():
 
 def createDownloadCardForPdf(path: str):
     file_name = path.split('/')[1]
-    return f"""<article><header>{file_name}</header><button role="button" class="secondary" onclick="download(\'{path}\')">Download</button>
-            <button role="button" class="outline contrary" onclick="deleteFile(\'{path}\')">Delete</button></article>"""
+    return f"""<article>
+            <header>{file_name}</header>
+            <button role="button" class="secondary" onclick="download(\'{path}\')">Download</button>
+            <button role="button" onclick="">Append</button>
+            <button role="button" class="outline contrary" onclick="deleteFile(\'{path}\')">Delete</button>
+            </article>"""
 
 
 @app.route('/download', methods=['POST'])
