58ef16e0d0a3c9e6c2f71d38bb10eb76fec2c8fa
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-07 09:51:56 +0100

    delete funktioniert

--- server.py
+++ server.py
@@ -11,17 +11,21 @@ def home():
 @app.route('/scanfunction', methods=['POST'])
 def scan_function():
     app.logger.info(request.get_json().get('type'))
-    pdf_files = glob.glob("scanRessources/*.pdf")
+    return createDownloadGrid()
     
+
+def createDownloadGrid():
+    pdf_files = glob.glob("scanRessources/*.pdf")
     html = ""
     for file in pdf_files:
         html += createDownloadCardForPdf(file)
     return html
+    
 
 def createDownloadCardForPdf(path: str):
     file_name = path.split('/')[1]
     return f"""<article><header>{file_name}</header><button role="button" class="secondary" onclick="download(\'{path}\')">Download</button>
-            <button role="button" class="outline contrary" onclick="delete(\'{path}\')">Delete</button></article>"""
+            <button role="button" class="outline contrary" onclick="deleteFile(\'{path}\')">Delete</button></article>"""
 
 
 @app.route('/download', methods=['POST'])
@@ -30,10 +34,10 @@ def download():
     app.logger.info(filepath)
     return send_file(filepath, as_attachment=True)
 
-@app.route('/delete', methods=['POST'])
+@app.route('/deleteFile', methods=['POST'])
 def delete():
     os.remove(request.get_json().get('filepath'))
-    return True
+    return createDownloadGrid()
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=5400, debug=True)
