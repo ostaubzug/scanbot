7a5b70238468b0200aa04d320cb7604c543ad6b3
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-18 22:11:18 +0100

    scale back on quality

--- scanRessources/scanDocument.sh
+++ scanRessources/scanDocument.sh
@@ -1,5 +1,5 @@
 current_date=$(date +%Y-%m-%d_%H-%M-%S)
-scanimage -d genesys --resolution=4800 --mode=Color --depth=16 --format=tiff -o scanRessources/scan_output${current_date}.tiff
+scanimage -d genesys --resolution=600 --mode=Color --depth=8 --format=tiff -o scanRessources/scan_output${current_date}.tiff
 tiff2pdf -o scanRessources/scan_output${current_date}.pdf scanRessources/scan_output${current_date}.tiff 
 #ocrmypdf scanRessources/scan_output${current_date}.pdf scanRessources/scan_${current_date}.pdf
 
