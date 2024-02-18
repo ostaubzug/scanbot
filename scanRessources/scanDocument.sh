1596da4cdab12e69f7493e58a9559c95f0eb8662
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-18 18:09:39 +0100

    where is my file?

--- scanRessources/scanDocument.sh
+++ scanRessources/scanDocument.sh
@@ -1,7 +1,7 @@
 current_date=$(date +%Y-%m-%d_%H-%M-%S)
 scanimage -d genesys:libusb:001:003 --format=pdf -o scan_output${current_date}.pdf 
-ocrmypdf scan_output${current_date}.pdf scan_${current_date}.pdf
+#ocrmypdf scan_output${current_date}.pdf scan_${current_date}.pdf
 
-rm scan_output${current_date}.pdf
+#rm scan_output${current_date}.pdf
 
 echo true
