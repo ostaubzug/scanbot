1917c9f2878965d1fe39bd30a55b00cc2f5fb547
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-18 22:03:29 +0100

    remove delete

--- scanRessources/scanDocument.sh
+++ scanRessources/scanDocument.sh
@@ -3,7 +3,7 @@ scanimage -d genesys --format=tiff -o scanRessources/scan_output${current_date}.
 #tiff2pdf -o scanRessources/scan_output${current_date}.pdf scanRessources/scan_output${current_date}.tiff 
 #ocrmypdf scanRessources/scan_output${current_date}.pdf scanRessources/scan_${current_date}.pdf
 
-rm scanRessources/scan_output${current_date}.pdf
-rm scanRessources/scan_output${current_date}.tiff
+#rm scanRessources/scan_output${current_date}.pdf
+#rm scanRessources/scan_output${current_date}.tiff
 
 echo true
