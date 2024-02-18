ea81dfa3c93c1507276385c297ca618507465721
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-18 18:28:51 +0100

    remove tiff

--- scanRessources/scanDocument.sh
+++ scanRessources/scanDocument.sh
@@ -3,6 +3,7 @@ scanimage -d genesys:libusb:001:003 --format=tiff -o scanRessources/scan_output$
 tiff2pdf -o scanRessources/scan_output${current_date}.pdf scanRessources/scan_output${current_date}.tiff 
 ocrmypdf scanRessources/scan_output${current_date}.pdf scanRessources/scan_${current_date}.pdf
 
-rm scan_output${current_date}.pdf
+rm scanRessources/scan_output${current_date}.pdf
+rm scanRessources/scan_output${current_date}.tiff
 
 echo true
