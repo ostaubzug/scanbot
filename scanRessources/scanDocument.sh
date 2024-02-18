c699d852f84de0afb7de6c227270d53c5cf5d1e5
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-18 22:08:43 +0100

    improve quality

--- scanRessources/scanDocument.sh
+++ scanRessources/scanDocument.sh
@@ -1,9 +1,9 @@
 current_date=$(date +%Y-%m-%d_%H-%M-%S)
-scanimage -d genesys --format=tiff -o scanRessources/scan_output${current_date}.tiff
-#tiff2pdf -o scanRessources/scan_output${current_date}.pdf scanRessources/scan_output${current_date}.tiff 
+scanimage -d genesys --resolution=4800 --mode=Color --depth=16 --format=tiff -o scanRessources/scan_output${current_date}.tiff
+tiff2pdf -o scanRessources/scan_output${current_date}.pdf scanRessources/scan_output${current_date}.tiff 
 #ocrmypdf scanRessources/scan_output${current_date}.pdf scanRessources/scan_${current_date}.pdf
 
 #rm scanRessources/scan_output${current_date}.pdf
-#rm scanRessources/scan_output${current_date}.tiff
+rm scanRessources/scan_output${current_date}.tiff
 
 echo true
