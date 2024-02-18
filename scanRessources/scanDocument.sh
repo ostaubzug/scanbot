d345503794be8a7c4f1812cbc936e39b237c9574
Staub Oliver I.BSCI_F22.2101 <oliver.staub@stud.hslu.ch>
2024-02-18 21:52:24 +0100

    adjust devicename

--- scanRessources/scanDocument.sh
+++ scanRessources/scanDocument.sh
@@ -1,5 +1,5 @@
 current_date=$(date +%Y-%m-%d_%H-%M-%S)
-scanimage -d genesys:libusb:001:003 --format=tiff -o scanRessources/scan_output${current_date}.tiff
+scanimage -d genesys --format=tiff -o scanRessources/scan_output${current_date}.tiff
 tiff2pdf -o scanRessources/scan_output${current_date}.pdf scanRessources/scan_output${current_date}.tiff 
 ocrmypdf scanRessources/scan_output${current_date}.pdf scanRessources/scan_${current_date}.pdf
 
